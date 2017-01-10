8897e3a61ef20223011148a3981e924693ed244c
shengnoah <tee168@qq.com>
2017-01-10 16:26:08 +0800

    添加sw_pt开关

--- log.lua
+++ log.lua
@@ -1,13 +1,22 @@
-sw_lp=0
+sw_pt=1
+sw_lp=1
 sw_ls=1
 sw_le=1
 sw_la=1
+
+
+function pt(...)
+    if sw_pt == 0 then
+        return
+    end
+    print(...)
+end
 
 function lp(...)
     if sw_lp == 0 then
         return
     end
-    print(...)
+    pt(...)
 end
 
 function ls(...)
@@ -18,7 +27,7 @@ function ls(...)
         fn_name=arg[1]
     end
  
-    print('=============== [s]:'..fn_name)
+    pt('=============== [s]:'..fn_name..' ===============')
 end
 
 function le(...)
@@ -27,14 +36,14 @@ function le(...)
     else
         fn_name=arg[1]
     end
-    print('=============== [e]:'..fn_name)
+    pt('=============== [e]:'..fn_name..' ===============') 
 end
 
 function ll(...)
-    print('---------------')
+    pt('---------------')
 end
 
 
 function la(...)
-    print(...)
+    pt(...)
 end
