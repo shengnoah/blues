9eed5c62e59de5c5f477ecee81bee286ca3c82fb
shengnoah <tee168@qq.com>
2021-10-29 17:11:23 +0800

    添加Heng的脚手架代码

---
+++ bin/scaffold/build/heng.lua
@@ -0,0 +1,15 @@
+local Heng = {}
+
+function  Heng.markdown2html( ... )
+    print("markdown2html")
+end
+
+function Heng.build( ... )
+    print("Heng: Build...")
+end
+
+function Heng.makePDF( ... )
+    print("Heng: Make PDF...")
+end
+
+return Heng
